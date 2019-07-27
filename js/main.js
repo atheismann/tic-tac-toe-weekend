dccfcea3f028b2984e80b0b5e8e449c5da40c816
Andrew Theismann <andrew@theismannconsulting.com>
2019-07-27 13:36:01 -0500

    Build click handler, winner handler, and final work

---
+++ js/main.js
@@ -0,0 +1,72 @@
+/*---- Constants ----*/
+const PLAYERS = {
+    "null" : "white",
+    "1" : "red",
+    "-1" : "blue",
+}
+ const WINNING_COMBOS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+ ]
+
+/*---- Define State Variables ----*/
+let board, turn, winner;
+
+/*---- Cached element references ----*/
+const msgEl = document.getElementById('msg');
+const sqEl = document.querySelectorAll('td div');
+
+/*---- Event listeners ----*/
+document.querySelector('table').addEventListener('click', handleClick);
+document.querySelector('button').addEventListener('click', init);
+
+/*---- Functions ----*/
+
+init();
+
+function init() {
+    board = [null, null, null, null, null, null, null, null, null];
+    turn = 1;
+    winner = null; 
+    render();
+
+}
+
+function render() {
+    board.forEach(function(sq, idx) {
+        sqEl[idx].style.background = PLAYERS[sq];
+    });
+    if(winner) {    
+        if (winner === 'T'){
+            msgEl.innerHTML = "Try Again, We Have A Tie!"
+        } else {
+            msgEl.innerHTML = `<span style="color:${PLAYERS[winner]}">${PLAYERS[winner].toUpperCase()}</span> Wins!`;
+        };
+    } else { 
+        msgEl.innerHTML = `<span style="color:${PLAYERS[turn]}">${PLAYERS[turn].toUpperCase()}</span>'s Turn!`;
+    };
+}
+
+function handleClick(evt) {
+    let idx = parseInt(evt.target.id.replace('sq', ''));
+    if (board[idx] || winner) return;
+    board[idx] = turn; 
+    turn *= -1;
+    winner = getWinner();
+    render();
+}
+
+function getWinner() {
+    for (let i = 0; i < WINNING_COMBOS.length; i++) {
+        if (Math.abs(board[WINNING_COMBOS[i][0]] + board[WINNING_COMBOS[i][1]] + board[WINNING_COMBOS[i][2]]) === 3) return board[WINNING_COMBOS[i][0]];
+    };
+    if (board.includes(null)) return null;
+    return 'T';
+}
+
